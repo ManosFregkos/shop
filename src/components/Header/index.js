8478ee3db661c52382e6190bf56d2d3fb06a80dc
eboxnet <manos@eboxnet.com>
2021-02-03 12:13:50 +0200

    initial app

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.scss";
+import Logo from "../../assets/logo.png";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="wrap">
+        <div className="logo">
+          <img src={Logo} alt="Simple Logo" />
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from "../../components/Directory/index";
+import "./styles.scss";
+
+export default function Homepage(props) {
+  return (
+    <section className="homepage">
+      <Directory />
+    </section>
+  );
+}
